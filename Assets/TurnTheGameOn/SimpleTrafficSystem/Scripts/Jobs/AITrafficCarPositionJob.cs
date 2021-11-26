9bcb5a1ff9e556b62452ded3ae1fa806450658ae
gutto19 <guga13@live.com>
2021-11-26 11:45:01 -0300

    tcc

---
+++ Assets/TurnTheGameOn/SimpleTrafficSystem/Scripts/Jobs/AITrafficCarPositionJob.cs
@@ -0,0 +1,19 @@
+﻿namespace TurnTheGameOn.SimpleTrafficSystem
+{
+    using UnityEngine;
+    using Unity.Collections;
+    using Unity.Burst;
+    using UnityEngine.Jobs;
+
+    public struct AITrafficCarPositionJob : IJobParallelForTransform
+    {
+        public NativeArray<Vector3> carTransformPositionArray;
+        public NativeArray<Vector3> carTransformPreviousPositionArray;
+
+        public void Execute(int index, TransformAccess carTransformAccessArray)
+        {
+            carTransformPreviousPositionArray[index] = carTransformPositionArray[index];
+            carTransformPositionArray[index] = carTransformAccessArray.position;
+        }
+    }
+}
